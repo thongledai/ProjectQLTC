448ba154ca5a6ab3a6ee30fc93b1653b1e498fb5
thientran24110336 <24110336@student.hcmute.edu.vn>
2025-12-24 10:52:44 +0700

    Update FileRepository.h

--- SourceCode/FileRepository.h
+++ SourceCode/FileRepository.h
@@ -13,14 +13,12 @@ class FileRepository {
 private:
     static json loadDatabase();
     static json* findUserNode(json& db, int userId);
-    static void saveDatabase(const json& db);
 
 public:
     static std::vector<User*> loadUsers();
-    // static void saveUsers(const std::vector<User*>& users);
-
     static std::vector<Account*> loadAccounts(int userId);
-    // static void saveAccounts(int userId, const std::vector<Account*>& accounts);
+
+    static std::vector<Transaction*> loadTransactionsByAccount(int userId, int accId);
 
     static void saveData(const vector<User*>& users);
 };
